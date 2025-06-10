5af11f73f2346d008f6741bdecd53ebd1f9d4aa0
000Jerry000 <whitewolf961024@gmail.com>
2025-06-10 18:11:42 +0900

    [ADD]: fadeIn effect in sections (HTML)

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,3 +1,4 @@
+// When width is smart phone screen, sp menu appears
 const btn = document.getElementById("toggleBtn");
 const body = document.getElementsByTagName("body")[0];
 
@@ -9,4 +10,24 @@ btn.addEventListener("click", () => {
     body.classList.remove("close");
     body.classList.add("open");
   }
+});
+// When scroll move, Fade in effect
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // Animate only once
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
